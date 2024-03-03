bf537ad0d70862d067484fcb5d83f26f0d5a51f1
Winq <103835411+winqdev@users.noreply.github.com>
2024-03-03 03:35:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-function disappear() {
-  var text = document.getElementById("contacth2");
-  text.style.animation = "blinker 0.2s step-start infinite";
-  text.style.opacity = 0;
-  setTimeout(function() {
-    text.style.display = "none";
-    location.href = "https://www.instagram.com/legion.mp2/";
-  }, 1000);
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+  var tracksText = document.getElementsByClassName('tracks');
+
+  fetch('https://api.legioq.lol/userinfo')
+  .then(response => response.json())
+  .then(data => {
+
+    if(data.tracks) {
+
+      for(let i = 0; i < 10; i++) {
+        var newTracksText = document.createElement('h3');
+        newTracksText.classList.add('tracks');
+        newTracksText.innerHTML = `<a href="${data.tracks[i].url}">${i + 1}. ${data.tracks[i].title}</a>`;
+
+        tracksText[1].parentNode.appendChild(newTracksText);
+      }
+    } else {
+      var newTracksText = document.createElement('h3');
+      newTracksText.classList.add('tracks');
+      newTracksText.textContent = '(Error while getting tracks)';
+
+      tracksText[0].textContent = 'ERROR: LEGIOQ_PLS_RELEASE_TRACK'
+      tracksText[1].parentNode.appendChild(newTracksText);
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    var newTracksText = document.createElement('h3');
+    newTracksText.classList.add('tracks');
+    newTracksText.textContent = '(Theres a fatal server error / Api server down)';
+
+    tracksText[0].textContent = 'ERROR: WINQDEV_SUCKS_IN_CODING'
+    tracksText[1].parentNode.appendChild(newTracksText);
+  });
+});
